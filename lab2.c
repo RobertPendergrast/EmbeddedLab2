f3f9fd17955f5dcf30c52070f9315195afbcd3c6
Moises Mata <mm6155@columbia.edu>
2025-03-03 17:37:57 -0500

    added networking formatting

--- lab2.c
+++ lab2.c
@@ -343,7 +343,7 @@ void *network_thread_f(void *ignored)
   while ( (n = read(sockfd, &recvBuf, BUFFER_SIZE - 1)) > 0 ) {
     recvBuf[n] = '\0';
     printf("%s", recvBuf);
-    clearline();
+    clearline(recvRow);
     fbputs(recvBuf, recvRow, 0);  
     recvRow++;
     if (recvRow >= USER_ROW - 1) {
