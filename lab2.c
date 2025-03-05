59958c07f9314a8449dfb53be3a4075ec4d77616
robertpendergrast <rlp2153@columbia.edu>
2025-03-05 15:47:50 -0500

    scrollin

--- lab2.c
+++ lab2.c
@@ -350,20 +350,15 @@ void print_message(char * message, int start_row, int cursor_pos){
     clearline(start_row+i);
   }
   printf("After Initial Clear Line\n");
-  int rows = (strlen(message)-1)/ROW_WIDTH + 1;
+  int rows = (strlen(message))/ROW_WIDTH + 1;
   printf("After STRLEN\n");
   //Clear the input section
   for(int i = 0; i < rows; i++){
     //clearline(start_row+i);
-    printf("Before Temp\n");
     char temp = message[(i+1)*ROW_WIDTH];
-    printf("After Temp\n");
     message[(i+1)*ROW_WIDTH] = '\0';
-    printf("After Message\n");
     fbputs(&(message[i*ROW_WIDTH]), start_row + i, 0,200,200,200);
-    printf("After fbputs\n");
     message[(i+1)*ROW_WIDTH] = temp;
-    printf("After Message\n");
   }
 }
 
